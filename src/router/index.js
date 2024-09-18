cb3a78fbb6b569535039d87a681c066ae7129c6c
fisheeesh <swanphyo444@gmail.com>
2024-09-18 15:03:53 +0700

    add global css style

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Welcome from '@/views/Welcome.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'welcome',
+    component: Welcome
   }
 ]
 
