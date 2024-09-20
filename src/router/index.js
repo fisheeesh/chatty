7d7830b6f6bd42ac8d584fd2bffe67dba69ab909
fisheeesh <swanphyo444@gmail.com>
2024-09-20 17:57:31 +0700

    real gangster shit

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,32 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Welcome from '@/views/Welcome.vue'
-import ChatRoom from '@/views/ChatRoom.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Welcome from "@/views/Welcome.vue";
+import ChatRoom from "@/views/ChatRoom.vue";
+import { auth } from "@/firebase/config";
 
 const routes = [
   {
-    path: '/',
-    name: 'welcome',
-    component: Welcome
+    path: "/",
+    name: "welcome",
+    component: Welcome,
   },
   {
-    path : '/chatroom',
-    name : 'chatroom',
-    component : ChatRoom
-  }
-]
+    path: "/chatroom",
+    name: "chatroom",
+    component: ChatRoom,
+    beforeEnter(to, from, next) {
+      let user = auth.currentUser;
+      if (user) {
+        next();
+      } else {
+        next({ name: "welcome" });
+      }
+    },
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
