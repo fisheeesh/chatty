4c139cc35c8c67e24c389d31ab0cb5dd98c04635
fisheeesh <swanphyo444@gmail.com>
2024-09-22 00:02:55 +0700

    use data-fns library for date format

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="chat-window">
         <div class="messages">
-            <div class="single" v-for="message in messages" :key="message.id">
-                <span class="created-at">{{ message.sent_at.toDate() }}</span>
+            <div class="single" v-for="message in formattedMessages" :key="message.id">
+                <span class="created-at">{{ message.sent_at }} ago</span>
                 <span class="name">{{ message.userName }}</span>
                 <span class="message">{{ message.message }}</span>
             </div>
@@ -12,25 +12,40 @@
 
 <script>
 import { db } from '@/firebase/config';
-import { ref } from 'vue';
+import { formatDistanceToNow } from 'date-fns';
+import { computed, ref } from 'vue';
 
 export default {
-    setup(){
-        let messages = ref([])
+    setup() {
+        let messages = ref([]) 
+        
+        /**
+         * When it comes to create a new data based on the original data, we use computed property
+         */
+        let formattedMessages = computed(() => {
+            return messages.value.map(msg => {
+                let formattedTime = formatDistanceToNow(msg.sent_at.toDate())
+                /**
+                 *  we can literally retun the msg obj, but we want to fix its sent_at field which we want to change its format
+                 * so we're gonna spread the incoming msg obj and override the sent_at field
+                 */
+                return { ...msg, sent_at: formattedTime } // {name, message, sent_at}
+            })// [{},{},{}]
+        })
         /**
          * onSnapshot() runs whenever there is any changes in the firestore database
          * That's why it is real time database
          */
         db.collection('messages').orderBy('sent_at').onSnapshot(snap => {
             let results = []
-            snap.docs.forEach(doc =>{
-                let document = {...doc.data(), id : doc.id}
+            snap.docs.forEach(doc => {
+                let document = { ...doc.data(), id: doc.id }
                 // console.log(document)
                 /**
-                 * If we dun catch with if condi, it will be null in the sent_at fiield cus we used timestamp()
-                 * timestamp takes a bit time to fetch data from firebase and our onSnapshot() fun before it gets the data
-                 * that's why sent_at field be null
-                 * So to overcome this, we will only add data when sent_at has data (which means we will wait for timestmap to complete)
+                 *! If we dun catch with if condi, it will be null in the sent_at fiield cus we used timestamp()
+                 *? timestamp takes a bit time to fetch data from firebase and our onSnapshot() fun before it gets the data
+                 *todo that's why sent_at field be null
+                 ** So to overcome this, we will only add data when sent_at has data (which means we will wait for timestmap to complete)
                  * and onSnapshot() will run again so in the output we will not face any problem
                  */
                 // if(doc.data().sent_at) results.push(document)
@@ -44,7 +59,7 @@ export default {
             messages.value = results
         })
 
-        return { messages }
+        return { messages, formattedMessages }
     }
 }
 </script>
@@ -54,24 +69,27 @@ export default {
     background: #f9fafe;
     padding: 30px 20px;
 }
+
 .messages {
     max-height: 400px;
     overflow: auto;
     /* background: red; */
 }
+
 .single {
     margin: 18px 0;
     /* background: green; */
 }
+
 .created-at {
     display: block;
     color: #999;
     font-size: 12px;
     margin-bottom: 4px;
 }
+
 .name {
     font-weight: bold;
     margin-right: 6px;
 }
-
 </style>
