20594a276dccb4dbe27713ac2703a83b8a0d2cbd
fisheeesh <swanphyo444@gmail.com>
2024-11-29 17:06:44 +0700

    chatroom design

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,52 @@
 <template>
-  
+    <section v-if="user"
+        class="navbar p-4 rounded-top-5 border-0 border-bottom bg-white d-flex justify-content-between align-items-center">
+        <!-- User Info -->
+        <div class="d-flex gap-3">
+            <img src="https://randomuser.me/api/portraits/men/1.jpg" alt="" width="50" class="img-fluid rounded-circle">
+            <div class="d-flex flex-column">
+                <span>Hi there, {{ user.displayName }}!</span>
+                <span class="fw-bold">Logged in as {{ user.email }}</span>
+            </div>
+        </div>
+        <!-- Logout button -->
+        <div>
+            <button type="button" data-bs-toggle="modal" data-bs-target="#logOutModal"
+                class="btn btn-primary rounded-5 px-3 py-2">Logout</button>
+        </div>
+    </section>
+    <!-- modal -->
+    <div class="modal fade" id="logOutModal">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title text-primary fw-bold fs-3" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p>Are you sure you want to log out of <strong class="text-primary">Ch</strong><strong
+                            class="text-secondary">a</strong><strong class="text-primary">tty</strong>?</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-dark" data-bs-dismiss="modal">Cancel</button>
+                    <button type="button" class="btn btn-primary" @click="signOut"
+                        data-bs-dismiss="modal">Logout</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
-<script>
+<script setup>
+import getUser from '@/composables/getUser';
+import useSignOUt from '@/composables/useSignOut';
 
+const { user } = getUser()
+const { error, logOut } = useSignOUt()
+
+const signOut = async () => {
+    await logOut()
+}
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/views/ChatRoom.vue
+++ src/views/ChatRoom.vue
@@ -1,22 +1,32 @@
 <template>
-  <section class="chatroom my-6 text-center">
-    <button @click="signOut" class="btn btn-primary btn-lg">LogOut</button>
+  <section class="chatroom my-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-10 offset-md-1 d-flex flex-column shadow-sm rounded-5 p-0">
+          <!-- Navbar -->
+          <NavBar />
+          <!-- ChatWindow -->
+          <div class="p-4 chat-window">
+          </div>
+          <!-- ChatForm -->
+          <div class="position-relative">
+            <textarea placeholder="Type a message and hit 'Enter' to send" class="form-control rounded-bottom-5 bg-white border-0 pt-3 pb-3 px-4"></textarea>
+            <i class="far fa-paper-plane position-absolute send"></i>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
 <script setup>
+import NavBar from '@/components/NavBar.vue';
 import getUser from '@/composables/getUser';
-import useSignOUt from '@/composables/useSignOut';
 import { watch } from 'vue';
 import { useRouter } from 'vue-router';
 
 const router = useRouter()
 const { user } = getUser()
-const { error, logOut } = useSignOUt()
-
-const signOut = async () => {
-  await logOut()
-}
 
 watch(user, () => {
   if (!user.value) router.push('/')
